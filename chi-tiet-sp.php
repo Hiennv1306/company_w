4f1ff5b15f8090b87e8709991ed618cda4f34437
PC <anhhien1306@gmail.com>
2019-01-12 11:21:41 +0700

    add html site index of company

---
+++ chi-tiet-sp.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Clip_vui</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale= 1, user-scalable=no">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/stylect.css">
+
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.theme.default.min.css">
+
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+	
+	
+</head>
+<body>
+	<header>
+		<div class="container-fluid">
+			<div class="row title-top">
+				<div class="col-md-2 col-md-offset-1 col-large-2 title-top1">
+					<div class="email">
+						<p><i class="fa fa-envelope" aria-hidden="true"></i> anhhien1306@gmail.com</p>
+					</div>
+				</div>
+				<div class="col-md-2 col-large-2">
+					<div class="icons">
+						<span><i class="fa fa-facebook" aria-hidden="true"></i></span>
+						<span><i class="fa fa-twitter" aria-hidden="true"></i></span>
+						<span><i class="fa fa-google" aria-hidden="true"></i></span>
+						<span><i class="fa fa-instagram" aria-hidden="true"></i></span>
+					</div>
+				</div>
+				<div class="col-md-2 col-md-offset-3 col-large-2 title-top1">
+					<div class="email">
+						<p><i class="fa fa-volume-control-phone" aria-hidden="true"></i> +12345667 66788</p>
+					</div>
+				</div>
+				<div class="col-md-2 col-large-2">
+					<div class="email " style="padding-left: 0;">
+						<p><i class="fa fa-clock-o" aria-hidden="true"></i></i>
+						8:00 - 23:00</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="navbar-wrapper ">
+		    <div class="container-fluid header">
+		        <nav class="navbar navbar-inverse navbar-static-top navheight">
+		            <div class="container conaito">
+		                <div class="navbar-header">
+		                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				                <span class="sr-only">Toggle navigation</span>
+				                <span class="icon-bar"></span>
+				                <span class="icon-bar"></span>
+				                <span class="icon-bar"></span>
+				            </button>
+		                    <a href="#"><img style="padding-top: 15px;" src="images/logo.png"></a>
+		                </div>
+		                <div id="navbar" class="navbar-collapse collapse list">
+		                    <ul class="nav navbar-nav menu">
+		                        <li><a href="#">Trang chủ</a></li>
+		                        <li><a href="#about">About</a></li>
+		                        <li><a href="#contact">Contact</a></li>
+		                        <li class="dropdown">
+		                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+		                            <ul class="dropdown-menu">
+		                                <li><a href="#">Action</a></li>
+		                                <li><a href="#">Another action</a></li>
+		                                <li><a href="#">Something else here</a></li>
+		                               
+		                            </ul>
+		                        </li>
+		                    </ul>
+		                </div>
+		            </div>
+		        </nav>
+
+		    </div>
+		</div>
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		    <ol class="carousel-indicators">
+		        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		        <li data-target="#myCarousel" data-slide-to="1"></li>
+		        <li data-target="#myCarousel" data-slide-to="2"></li>
+		    </ol>
+		    <div class="carousel-inner" role="listbox">
+		        <div class="item active">
+		            <img style="width: 100%;" class="first-slide" src="images/banner.jpg" alt="First slide">
+		            <div class="container">
+		                <div class="carousel-caption">
+		                    <h1>Example headline.</h1>
+		                    <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
+		                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Sign up today</a></p>
+		                </div>
+		            </div>
+		        </div>
+		        <div class="item">
+		            <img style="width: 100%;" class="second-slide" src="images/banner.jpg" alt="Second slide">
+		            <div class="container">
+		                <div class="carousel-caption">
+		                    <h1>Another example headline.</h1>
+		                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Learn more</a></p>
+		                </div>
+		            </div>
+		        </div>
+		        <div class="item">
+		            <img style="width: 100%;" class="third-slide" src="images/banner.jpg" alt="Third slide">
+		            <div class="container">
+		                <div class="carousel-caption">
+		                    <h1>One more for good measure.</h1>
+		                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+		                    <p><a class="btn btn-lg btn-primary" href="#" role="button">Browse gallery</a></p>
+		                </div>
+		            </div>
+		        </div>
+		    </div>
+		    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+		        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		        <span class="sr-only">Previous</span>
+		      </a>
+		    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+		        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		        <span class="sr-only">Next</span>
+		      </a>
+		</div>
+	</header>
+	<content>
+		<div class="container">
+			<div class="row">
+				<div id="viver-content" class="single">
+				    <div class="container bs">
+				        <div class="block-products">
+				            <div class="block-content product-container row bs">
+				                <div class="row mg-t20 row-m10">
+				                    <div class="col-xs-12 col-sm-6 mg-b10">
+				                        <div class="product-images">
+				                           <img src="images/anh1.png">
+				                        </div>
+				                    </div>
+				                    <div class="col-xs-12 col-sm-6 mg-b10">
+				                        <div class="block-title title-bo">
+				                            <h1 class="title"><span>ĐÈN THẢ HIỆN ĐẠI FILLSTA 35CM SV_FST35</span></h1>
+				                        </div>
+				                        <div class="product-box single-content">
+				                            <div class="desc">
+				                                <div class="product-code">
+				                                    Mã sản phẩm: <span></span>
+				                                </div>
+				                                <div class="price-big">Liên hệ</div>
+				                                <div class="sub-title">Đặc điểm nổi bật:</div>
+				                            </div>
+				                        </div>
+				                        <div class="product-footer row bs">
+				                            <a href="http://noithatsongvu.com.vn/cart/add?pid=86" class="btn-mk btn-red"><i class="fa fa-book"></i>Mua hàng</a>
+
+				                            <div class="icon-phone">
+				                                <div class="text">
+				                                    <span class="f">Hotline CSKH</span>
+				                                    <span class="s">0966 633 000 - 0948 651 668</span>
+				                                </div>
+				                            </div>
+				                        </div>
+				                    </div>
+				                    <div class="col-xs-12 col-sm-9 mg-b10">
+				                        <div class="single-content bo">
+				                            <div class="block-title title-bo">
+				                                <h2 class="title">Thông tin sản phẩm</h2>
+				                            </div>
+				                            <p><strong>Bộ đèn thả Fillsta 35cm SV_FST35</strong>&nbsp;là một thiết kế cao cấp thuộc dòng đèn thả, vừa để chiếu sáng, vừa để trang trí không gian, đồng thời mang lại nhiều lợi ích thiết thực khác.</p>
+
+				                            <p><strong>ƯU ĐIỂM CỦA ĐÈN THẢ FILLSTA 35CM</strong></p>
+
+				                            <ul>
+				                                <li>Thiết kế đèn tinh xảo, chịu được các tác động của ngoại lực.</li>
+				                                <li>Đèn phát sáng ngay lập tức khi bật công tắc, không bị nhấp nháy khi giơ camera quay video hoặc chụp ảnh.</li>
+				                                <li>Tuổi thọ lớn, độ bền cao, giúp giảm chi phí sử dụng vì bạn không phải thay mới đèn thường xuyên giống.</li>
+				                                <li>Cấu tạo nhỏ gọn, dễ dàng di chuyển và lắp đặt, tránh méo mó khi va đập.</li>
+				                                <li>Đèn không chứa các chất độc hại như thủy ngân, chì, thân thiện với môi trường.</li>
+				                            </ul>
+
+				                            <p><img alt="" src="/assets/uploads/images/den-tha-hien-dai-fillsta-35cm-ctht-152-p266.png" style="border-width: 1px; border-style: solid; margin: 3px; width: 650px; height: 650px;"></p>
+
+				                            <p><strong>ỨNG DỤNG PHỔ BIẾN</strong></p>
+
+				                            <ul>
+				                                <li>Trang trí cho những không gian như phòng khách, phòng ngủ, ứng dụng cho không gian nội thất nhà ở.</li>
+				                                <li>Chiếu sáng cho bên trong các trung tâm thương mại, chung cư, sử dụng đèn led ốp nối tròn&nbsp;với bất kì những không gian sang trọng và rộng rãi, các công trình dự án lớn.</li>
+				                                <li>Làm nổi bật cho các quán cà phê, shop, showroom…</li>
+				                            </ul>
+				                        </div>
+				                    </div>
+
+				                    <div class="col-xs-12 col-sm-3">
+				                        <div class="box box-products">
+				                            <div class="box-title title-bg">
+				                                <span class="title">Sản phẩm liên quan</span>
+				                            </div>
+				                            <div class="box-content bo in-5">
+				                                <div class="ii-product xxs ptb-5">
+				                                    <div class="inner">
+				                                        <div class="row row-m5">
+				                                            <div class="image fit col-xxs-12 col-xs-4">
+				                                                <a href="http://noithatsongvu.com.vn/den-tha-le-klint-30cm-sv-lek30.html" title="ĐÈN THẢ LE KLINT 30CM SV_LEK30"><img src="http://noithatsongvu.com.vn//assets/thumbs/assets_uploads_images_TB28wrywm0mpuFjSZPiXXbssVXa_0-rate.jpg_400x400_34263_100x100.jpg" alt="ĐÈN THẢ LE KLINT 30CM SV_LEK30" title="ĐÈN THẢ LE KLINT 30CM SV_LEK30"></a>                                                </div>
+				                                            <div class="col-xxs-12 col-xs-8">
+				                                                <div class="title"><a href="http://noithatsongvu.com.vn/den-tha-le-klint-30cm-sv-lek30.html">ĐÈN THẢ LE KLINT 30CM SV_LEK30</a></div>
+				                                                <div class="price">
+				                                                    Liên hệ </div>
+				                                            </div>
+				                                        </div>
+				                                    </div>
+				                                </div>
+				                                <div class="ii-product xxs ptb-5">
+				                                    <div class="inner">
+				                                        <div class="row row-m5">
+				                                            <div class="image fit col-xxs-12 col-xs-4">
+				                                                <a href="http://noithatsongvu.com.vn/den-tha-knappa-35x35cm-sv-knpa35.html" title="ĐÈN THẢ KNAPPA 35X35CM SV_KNPA35"><img src="http://noithatsongvu.com.vn//assets/thumbs/assets_uploads_images_den-tha-knappa-35x35cm-cth-155-p267_160278_100x100.png" alt="ĐÈN THẢ KNAPPA 35X35CM SV_KNPA35" title="ĐÈN THẢ KNAPPA 35X35CM SV_KNPA35"></a>                                                </div>
+				                                            <div class="col-xxs-12 col-xs-8">
+				                                                <div class="title"><a href="http://noithatsongvu.com.vn/den-tha-knappa-35x35cm-sv-knpa35.html">ĐÈN THẢ KNAPPA 35X35CM SV_KNPA35</a></div>
+				                                                <div class="price">
+				                                                    Liên hệ </div>
+				                                            </div>
+				                                        </div>
+				                                    </div>
+				                                </div>
+				                                <div class="ii-product xxs ptb-5">
+				                                    <div class="inner">
+				                                        <div class="row row-m5">
+				                                            <div class="image fit col-xxs-12 col-xs-4">
+				                                                <a href="http://noithatsongvu.com.vn/den-tha-hien-dai-sv-thd01.html" title="ĐÈN THẢ HIỆN ĐẠI SV_THD01"><img src="http://noithatsongvu.com.vn//assets/thumbs/assets_uploads_images_den-tha-hien-dai-cdt15-p94_553443_100x100.png" alt="ĐÈN THẢ HIỆN ĐẠI SV_THD01" title="ĐÈN THẢ HIỆN ĐẠI SV_THD01"></a>                                                </div>
+				                                            <div class="col-xxs-12 col-xs-8">
+				                                                <div class="title"><a href="http://noithatsongvu.com.vn/den-tha-hien-dai-sv-thd01.html">ĐÈN THẢ HIỆN ĐẠI SV_THD01</a></div>
+				                                                <div class="price">
+				                                                    Liên hệ </div>
+				                                            </div>
+				                                        </div>
+				                                    </div>
+				                                </div>
+				                                <div class="ii-product xxs ptb-5">
+				                                    <div class="inner">
+				                                        <div class="row row-m5">
+				                                            <div class="image fit col-xxs-12 col-xs-4">
+				                                                <a href="http://noithatsongvu.com.vn/den-tha-go-sv-tg01.html" title="ĐÈN THẢ GỖ SV_TG01"><img src="http://noithatsongvu.com.vn//assets/thumbs/assets_uploads_images_den-tha-go-ma-cth-264-p336_159624_100x100.png" alt="ĐÈN THẢ GỖ SV_TG01" title="ĐÈN THẢ GỖ SV_TG01"></a>                                                </div>
+				                                            <div class="col-xxs-12 col-xs-8">
+				                                                <div class="title"><a href="http://noithatsongvu.com.vn/den-tha-go-sv-tg01.html">ĐÈN THẢ GỖ SV_TG01</a></div>
+				                                                <div class="price">
+				                                                    Liên hệ </div>
+				                                            </div>
+				                                        </div>
+				                                    </div>
+				                                </div>
+				                            </div>
+				                        </div>
+				                    </div>
+				                </div>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+			</div>
+
+		</div>
+	</content>
+	<footer>
+		<div class="container max">
+			<div class="row footer">
+				<div class="col-md-8 col-large-8">
+					<div class="row">
+						<div class="col-md-6  col-large-6">
+							<div class="logo">
+								<img src="images/logo-footer.png">
+								<p>psum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type tras</p>
+							</div>
+						</div>
+						<div class="col-md-6 col-large-6 info">
+							<h4>CÔNG TY TNHH DỊCH VỤ TRUYỀN THÔNG QUẢNG CÁO T&G</h4>
+							<p>Địa chỉ: 142 Nguyễn Ngọc Nại<br>
+							   Phone: 1235665<br>
+							   Email: anhieno@gmail.com<br>
+							   Website : ngunguoi.vn
+							</p>
+						</div>
+						<div class="col-md-12 col-large-12 map">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.8975751642038!2d105.83245601440751!3d21.036783892881388!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135aba15ec15d17%3A0x620e85c2cfe14d4c!2sHo+Chi+Minh+Mausoleum!5e0!3m2!1sen!2s!4v1543640466622" width="100%" height="180" frameborder="0" style="border:0" allowfullscreen></iframe>
+						</div>
+					</div>
+					
+				</div>
+				
+				<div class="col-md-4 col-large-4">
+					<div class="page">
+						<div class="fb-page" data-href="https://www.facebook.com/noithaththome/" data-tabs="timeline" data-width="350" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/noithaththome/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/noithaththome/">HT&#039;home - Thiết kế Thi công nội thất</a></blockquote></div>
+					</div>
+				</div>
+				<!-- Load Facebook SDK for JavaScript -->
+				<div id="fb-root"></div>
+				<script>(function(d, s, id) {
+				  var js, fjs = d.getElementsByTagName(s)[0];
+				  if (d.getElementById(id)) return;
+				  js = d.createElement(s); js.id = id;
+				  js.src = 'https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v3.2';
+				  fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));</script>
+
+
+			</div>
+		</div>
+	</footer>
+	
+	
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/owl.carousel.min.js"></script>
+	
+
+</body>
+</html>
